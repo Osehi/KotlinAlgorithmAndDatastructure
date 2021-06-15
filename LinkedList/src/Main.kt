acba54db224a8d9e8ffaed4d7267209246002f83
Osehi <osehiaseehilen@gmail.com>
2021-06-15 09:03:51 +0100

    main function added

---
+++ LinkedList/src/Main.kt
@@ -0,0 +1,22 @@
+
+fun main(){
+//    val node1 = Node(1)
+//    val node2 = Node(2)
+//    val node3 = Node(3)
+//    node1.next = node2
+//    node2.next = node3
+//    println(node1)
+    val list = LinkedList<Int>()
+    list.push(0).push(1).push(2)
+
+//    println(list.reverseLinkedList())
+        println(list)
+        print("reverseLinkedList")
+        println(list.reverseLinkedList())
+//    println(list)
+
+//    val appendList = LinkedList<Int>()
+//    appendList.append(0)
+//
+//    println(appendList)
+}
